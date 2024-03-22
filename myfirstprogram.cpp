3ff7bfb27ed8497e1520c1116aa72d0e525aebab
James Asira <jjasira2016@gmail.com>
2024-03-22 17:22:22 +0300

    learnt about variable types, declaration and assigning in C++

--- myfirstprogram.cpp
+++ myfirstprogram.cpp
@@ -3,6 +3,13 @@ using namespace std;
 
 int main()
 {
-    cout << "hello World!";
+    // using the \n escape character to start a new line
+    cout << "hello World! \n";
+    // using the endl keyword in order to start a new line
+    cout << "I love C++" << endl;
+    /*
+    writing mulitple line comments
+    */
+    cout << "Join me";
     return 0;
 }
---
+++ variables.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // to declare more than one variable of the same type, use a comma-separated list
+    int myNum = 15, myNum2 = 11;
+    int sum = myNum + myNum2;
+    cout << "I am " << sum << " years old";
+    return 0;
+}
